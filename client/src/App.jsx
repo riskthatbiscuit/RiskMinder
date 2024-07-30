d76dd4e7a80b80ee1f89de1f4f04e96e05783c76
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-07-30 17:46:37 -0400

    Resetting client side http route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 const httpLink = createHttpLink({
-  uri: MONGODB_URI || '/graphql',
+  uri: process.env.MONGODB_URI || '/graphql',
 })
 
 const authLink = setContext((_, { headers }) => {
