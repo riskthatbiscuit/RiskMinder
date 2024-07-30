c0f87cf80e45922cb2fa651735435d49278f57f4
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-07-30 17:19:33 -0400

    Resetting client side http route

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
+console.log('MONGODB_URI:', process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/riskminderDB");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/riskminderDB");
+mongoose.connect(process.env.MONGODB_URI)
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const { expressMiddleware } = require('@apollo/server/express4')
 const { authMiddleware } = require('./utils/auth')
 const path = require('path')
 const { typeDefs, resolvers } = require('./schemas')
-const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3001
+const db = require('./config/connection')
 
 const app = express()
 
@@ -13,42 +13,34 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
-
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-  await server.start()
+  await server.start();
 
-  app.use(express.urlencoded({ extended: false }))
-  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')))
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-    })
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   }
 
-  app.use(
-    '/graphql',
-    expressMiddleware(server, {
-      context: authMiddleware,
-    }),
-  )
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
 
-  const MONGODB_URI = process.env.MONGODB_URI || 'https://localhost:27017/RiskMinderDB'
-
-  mongoose
-    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`)
-        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
-      })
-    })
-    .catch((err) => {
-      console.error('Failed to connect to MongoDB', err)
-    })
-}
+// Call the async function to start the server
 
 startApolloServer()
 
